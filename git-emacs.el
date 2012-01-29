d30657fcb53510a49be5d4a22c408652c499a8c6
Taesoo <tsgatesv@gmail.com>
2012-01-29 10:42:16 -0500

    fix names

--- git-emacs.el
+++ git-emacs.el
@@ -1,15 +1,15 @@
 ;;; git-emacs (v.1.4.3) : yet another git emacs mode for newbies
 ;;
-;; Copyright (C) 2008  TSKim (tsgatesv@gmail.com)
+;; Copyright (C) 2008  Taesoo Kim (tsgatesv@gmail.com)
 ;;
 ;; v.1.4.3 Modified by ovy            @ 20 September 2009
 ;; v.1.4   Modified by ovy            @ 22 March 2009
 ;; v.1.3   Modified by Con Digitalpit @ 29 March 2008
 ;;
-;; Authors    : TSKim : Kim Taesoo(tsgatesv@gmail.com)
-;; Created    : 24 March 2007
-;; License    : GPL
-;; Keywords   : git, version control, release management
+;; Authors:  Taesoo Kim <tsgatesv@gmail.com>
+;; Created:  24 March 2007
+;; License:  GPL
+;; Keywords: git, version control, release management
 ;;
 ;; Compatibility: Emacs22 and EmacsCVS (developed on 23.0.60.2)
 ;;                Git 1.5 and up
